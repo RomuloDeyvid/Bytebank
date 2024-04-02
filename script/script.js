d8e21e262fa2ae48e7445c393cea7ad67673984d
Rômulo Deyvid <romulo.deyvid@hotmail.com>
2024-04-02 11:08:43 -0300

    Adicionando os gráficos da cotação de Dólar e Iene

--- script/script.js
+++ script/script.js
@@ -1,29 +1,68 @@
+import selecionaCotacao from "./imprimeCotacao.js";
+import imprimeCotacao from "./imprimeCotacao.js";
 const graficoDolar = document.querySelector('#graficoDolar')
+const graficoIene = document.querySelector('#graficoIene')
 
 const graficoParaDolar = new Chart(graficoDolar, {
-    type: 'line',
-    data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1
-      }]
-    },
-  });
+  type: 'line',
+  data: {
+    labels: [],
+    datasets: [{
+      label: 'Dolar',
+      data: [],
+      borderWidth: 1
+    }]
+  },
+});
 
-async function conectaAPI(){
-    const conecta = await fetch('https://economia.awesomeapi.com.br/json/last/USD-BRL')
-    const conectaConvertido = await conecta.json()
-    console.log(conectaConvertido)
-}
+const graficoParaIene = new Chart(graficoIene, {
+  type: 'line',
+  data: {
+    labels: [],
+    datasets: [{
+      label: 'Iene',
+      data: [],
+      borderWidth: 1
+    }]
+  },
+});
 
-setInterval(() => conectaAPI(), 5000)
-
-function geraHorario(){
-    let data = new Date()
-    let horario = data.getHours() + ':' + data.getMinutes() + ':' + data.getSeconds()
-    console.log(horario)
-    return horario
+function geraHorario() {
+  let data = new Date()
+  let horario = data.getHours() + ':' + data.getMinutes() + ':' + data.getSeconds()
+  
+  return horario
 }
 geraHorario()
+
+function adicionarDados(grafico, legenda, dados) {
+  grafico.data.labels.push(legenda)
+  grafico.data.datasets.forEach((dataset) => {
+    dataset.data.push(dados)
+  });
+
+  grafico.update()
+}
+
+let workerDolar = new Worker('./script/workers/workerDolar.js')
+
+workerDolar.postMessage('usd')
+
+workerDolar.addEventListener('message', event =>{
+  let tempo = geraHorario()
+  let valor = event.data.ask
+  
+  adicionarDados(graficoParaDolar, tempo, valor)
+  selecionaCotacao('dolar', valor)
+})
+
+let workerIene = new Worker('./script/workers/workerIene.js')
+workerIene.postMessage('iene')
+
+workerIene.addEventListener('message', event =>{
+  let tempo = geraHorario()
+  let valor = event.data.ask
+  
+  adicionarDados(graficoParaIene, tempo, valor)
+  selecionaCotacao('iene', valor)
+})
