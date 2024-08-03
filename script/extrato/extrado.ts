2b32b1c85dbd2dea4e1a883cfe1f761d753658ce
Rômulo Deyvid <romulo.deyvid@hotmail.com>
2024-08-03 17:15:36 -0300

    Adicionando pagina de extrato para praticar typescript

---
+++ script/extrato/extrado.ts
@@ -0,0 +1,45 @@
+let saldo = 3000;
+
+const elementoSaldo = document.querySelector('.saldo-valor .valor') as HTMLElement;
+
+if(elementoSaldo != null){
+    elementoSaldo.textContent = saldo.toString();
+}
+
+
+const elementoFormulario = document.querySelector('.block-nova-transacao form') as HTMLFormElement;
+
+elementoFormulario.addEventListener('submit', function (event) {
+    event.preventDefault();
+    if (!elementoFormulario.checkValidity()) {
+        alert('oi');
+        return;
+    }
+    
+    const inputTipoTransacao = document.querySelector('#tipoTransacao') as HTMLSelectElement
+    const inputValor = document.querySelector('#valor') as HTMLInputElement
+    const inputData = document.querySelector('#data') as HTMLInputElement
+
+    let tipoTransacao : string= inputTipoTransacao.value
+    let valor : number = inputValor.valueAsNumber
+    let data : Date = new Date(inputData.value) 
+
+    if(tipoTransacao == 'Depósito'){
+        saldo += valor
+        
+    }else if (tipoTransacao == 'Transferência' || tipoTransacao == 'Pagamento de Boleto') {
+        saldo -= valor
+        console.log(`Novo saldo após ${tipoTransacao}: ${saldo}`);
+    }else{
+        alert('Tipo de transação inválida')
+    }
+
+    const novaTransacao = {
+        tipoTransacao,
+        valor,
+        data
+    }
+    console.log(novaTransacao)
+    elementoSaldo.textContent = saldo.toString();
+
+});
